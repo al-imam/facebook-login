c4928b81643ef66df4e7c55d2d1a51c9baa63c15
al-imam <mdalimam01828@gmail.com>
2022-11-24 22:34:19 +0600

    complete form ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,73 @@
-import React from "react"
+import React, { useReducer } from "react";
+import classes from "./login.module.css";
 
-const Login: React.FunctionComponent = () => {
-  return <div>Form</div>
+interface State {
+  username: string;
+  password: string;
 }
 
-export default Login
+const init: State = {
+  username: "",
+  password: "",
+};
+
+interface Action {
+  type: "password" | "username";
+  payload: string;
+}
+
+type RF = (state: State, action: Action) => State;
+
+const reduce: RF = (state, action)=> {
+  const { type, payload } = action;
+  switch (type) {
+    case "password":
+      return {
+        ...state,
+        password: payload,
+      };
+    case "username":
+      return {
+        ...state,
+        username: payload,
+      };
+    default:
+      throw new Error("invalid action");
+  }
+};
+
+const Login: React.FunctionComponent = () => {
+  const [{ username, password }, dispatch] = useReducer(reduce, init);
+  return (
+    <form className={classes.form}>
+      <input
+        className={classes.input}
+        type="text"
+        onChange={(evt) =>
+          dispatch({ type: "username", payload: evt.target.value })
+        }
+        value={username}
+        placeholder="Email address or phone number"
+        required
+      />
+      <input
+        className={classes.input}
+        type="password"
+        onChange={(evt) =>
+          dispatch({ type: "password", payload: evt.target.value })
+        }
+        value={password}
+        placeholder="Password"
+        required
+      />
+      <button className={classes.log} type="submit">
+        Log in
+      </button>
+      <a href="#" className={classes.forget}>Forget password?</a>
+      <hr className={classes.line}/>
+      <button className={classes.newAccount} type="button">Create New Account</button>
+    </form>
+  );
+};
+
+export default Login;
