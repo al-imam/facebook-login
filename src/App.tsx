8fd0f090347b2d28c0fcf737c69183fc5cabd3fb
al-imam <mdalimam01828@gmail.com>
2022-11-24 14:55:13 +0600

    create app.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>noting here!</div>;
-}
+import classes from "./app.module.css";
+import Login from "./components/Login";
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className={classes.body}>
+     <h1>Facebook</h1>
+     <span>Facebook helps you connect and share with the people in your life.</span>
+      <Login />
+    </div>
+  );
+};
 
 export default App;
